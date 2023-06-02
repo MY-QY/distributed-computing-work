0b6436fe1e89f894f104d6c513ba0b173d6e1c90
akynazh <akynazh@qq.com>
2023-06-02 17:55:02 +0800

    format

--- hadoop_spark/work/p1.py
+++ hadoop_spark/work/p1.py
@@ -8,15 +8,24 @@ input_file = spark.read.text("/input/p1_input_file.txt")
 df = input_file.selectExpr("split(value, ',') as data")
 
 # 将 DataFrame 转换为 RDD
-rdd = df.rdd.map(lambda row: (row.data[0], row.data[1], row.data[2], row.data[3], float(row.data[4])))
+rdd = df.rdd.map(
+    lambda row: (row.data[0], row.data[1], row.data[2], row.data[3], float(row.data[4]))
+)
 
 # 计算每个学生必修课的平均成绩
-student_avg_scores = rdd.filter(lambda row: row[3] == "必修").map(lambda row: ((row[0], row[1]), row[4])) \
-    .groupByKey().mapValues(lambda scores: sum(scores) / len(scores))
+student_avg_scores = (
+    rdd.filter(lambda row: row[3] == "必修")
+    .map(lambda row: ((row[0], row[1]), row[4]))
+    .groupByKey()
+    .mapValues(lambda scores: sum(scores) / len(scores))
+)
 
 # 按科目统计每个班的平均成绩
-class_subject_avg_scores = rdd.map(lambda row: ((row[0], row[2]), row[4])) \
-    .groupByKey().mapValues(lambda scores: sum(scores) / len(scores))
+class_subject_avg_scores = (
+    rdd.map(lambda row: ((row[0], row[2]), row[4]))
+    .groupByKey()
+    .mapValues(lambda scores: sum(scores) / len(scores))
+)
 
 # 保存结果到文件
 student_avg_scores.saveAsTextFile("/output/student_avg_scores")
--- hadoop_spark/work/p2.py
+++ hadoop_spark/work/p2.py
@@ -6,10 +6,19 @@ input_file = sc.textFile("/input/p2_input_file.txt")
 
 relationships = input_file.map(lambda line: line.split(","))
 parent_child_pairs = relationships.map(lambda pair: (pair[0].strip(), pair[1].strip()))
-mid_pairs = parent_child_pairs.flatMap(lambda pair: [(pair[1], pair[0], 1), (pair[0], pair[1], 0)])
+mid_pairs = parent_child_pairs.flatMap(
+    lambda pair: [(pair[1], pair[0], 1), (pair[0], pair[1], 0)]
+)
 grouped_pairs = mid_pairs.groupBy(lambda pair: pair[0])
 # res_pairs = grouped_pairs.flatMap(lambda group: [(pair1[1], pair2[1]) for pair1 in group[1] for pair2 in group[1] if pair1[2] == 1 and pair2[2] == 0])
-res_pairs = grouped_pairs.flatMap(lambda group: [(pair2[1], pair1[1]) for pair1 in group[1] for pair2 in group[1] if pair1[2] == 1 and pair2[2] == 0])
+res_pairs = grouped_pairs.flatMap(
+    lambda group: [
+        (pair2[1], pair1[1])
+        for pair1 in group[1]
+        for pair2 in group[1]
+        if pair1[2] == 1 and pair2[2] == 0
+    ]
+)
 res_pairs.saveAsTextFile("/output/grandchild_grandparent_groups")
 
 # spark-submit p2.py
